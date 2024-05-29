fb1f7ff965a91e09ceaf5efe1c6c831fa5a6a905
Paiaro <gabrielpaiarop@gmail.com>
2024-05-29 15:23:09 -0300

    Create conecta.php

---
+++ inc/conecta.php
@@ -0,0 +1,28 @@
+<?php
+// Script de conexão com o banco de dados
+
+
+// parametros para conexão
+$servidor = "localhost";
+$usuario =  "root";
+$senha = "";
+$banco = "microblog_paiaro";
+
+
+//função para conexão com o servidor de banco de dados.
+
+$conexao = mysqli_connect($servidor,$usuario,$senha, $banco);
+
+// definindo o charset da conexão para utf8
+
+mysqli_set_charset($conexao, "utf8");
+
+// fazendo teste de conexão
+
+if (!$conexao) {
+    // comando die "mata a aplicação" fazendo ela parar.
+    die("WASTED" .mysqli_connect_error());
+}else{
+echo "deu certo";
+
+}
