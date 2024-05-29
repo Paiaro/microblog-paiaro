8881e68fadded73827ce653d68e6e14da1fc15b7
Paiaro <gabrielpaiarop@gmail.com>
2024-05-29 16:13:58 -0300

    Update funcoes-usuarios.php

--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -2,11 +2,11 @@
 // acesso aos dados da conexão do servidor
 require "conecta.php";
 
-function inserirUsuario($conexao, $nome, $email, $tipo, $senha ){
+function inserirUsuario($conexao, $nome, $email, $tipo, $senha)
+{
     // montando o comando SQL em uma variavel
     $sql = "INSERT INTO usuarios (nome, email, tipo, senha) VALUES ('$nome', '$email', '$tipo','$senha')";
 
     // Executando o comando no banco
-    mysqli_query($conexao,$sql) or die(mysqli_error($conexao));
-
+    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 }
