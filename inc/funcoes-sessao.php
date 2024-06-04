2bf651a9627a36049966650167a7d1c17f47f0ed
Paiaro <gabrielpaiarop@gmail.com>
2024-06-04 17:11:47 -0300

    Update funcoes-sessao.php

---
+++ inc/funcoes-sessao.php
@@ -0,0 +1,7 @@
+<?php 
+
+// sessoes no php. Recurso usado para controle de acesso à determinadas páginas e/ou recursos do site.
+// Exemplos : area admin, painel de controle, perfil de usuario
+// nestas areas o acesso só é possivel mediante alguma forma de autenticação : login, senha,digital, facil.
+
+
