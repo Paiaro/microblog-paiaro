23c0ec0b709833c889ced2fecef28e84d2671404
Paiaro <gabrielpaiarop@gmail.com>
2024-06-04 16:13:46 -0300

    Update funcoes-usuarios.php

--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -36,5 +36,6 @@ function lerUmUsuario($conexao, $id){
 }
 function atualizarUsuario($conexao, $id, $nome, $email, $senha, $tipo){
 
-    
+    $sql = "UPDATE usuarios SET nome = '$nome', email = '$email', senha = '$senha', tipo = '$tipo' WHERE id = $id ";
+    mysqli_query($conexao,$sql) or die (mysqli_error($conexao));
 }
