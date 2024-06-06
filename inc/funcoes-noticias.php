f072b72726ef7573405f407ef0a36784b1b6f841
Paiaro <gabrielpaiarop@gmail.com>
2024-06-06 13:57:01 -0300

    Create funcoes-noticias.php

---
+++ inc/funcoes-noticias.php
@@ -0,0 +1,17 @@
+<?php
+
+require "conecta.php";
+
+function inserirNoticias($conexão){ 
+}
+
+function lerNoticias($conexão){ 
+}
+
+function lerUmaNoticia($conexão){ 
+}
+
+function atualizarNoticia($conexão){ 
+}
+function excluirNoticia($conexão){ 
+}
