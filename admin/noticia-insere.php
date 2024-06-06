e1d673089ef44e1a5a133e285ee8c7ef74f7ef26
Paiaro <gabrielpaiarop@gmail.com>
2024-06-06 14:27:41 -0300

    Update noticia-insere.php

--- admin/noticia-insere.php
+++ admin/noticia-insere.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
 require_once "../inc/cabecalho-admin.php";
 require_once "../inc/funcoes-noticias.php";
 
-if (isset ($_POST['inserir'])) {
+if (isset($_POST['inserir'])) {
 	$titulo = $_POST['titulo'];
 	$texto = $_POST['texto'];
 	$resumo = $_POST['resumo'];
 
 	// obtendo o id do usuario que esta logado e inserindo a noticia. portanto, a noticia sera acossiada ao usuario devido ao uso de chave estrangeira e relacionamento no banco.
 	$usuarioId = $_SESSION['id'];
-// Capturando dados do arquivo enviado.
+	// Capturando dados do arquivo enviado.
 	$imagem = $_FILES['imagem'];
 
 	echo "<pre>";
@@ -27,44 +27,42 @@ if (isset ($_POST['inserir'])) {
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
+
 		<h2 class="text-center">
-		Inserir nova notícia
+			Inserir nova notícia
 		</h2>
-				<!-- O atributo enctype com o valor indicado é necessario quando queremos que o formulario aceite o envio de arquivos de qualquer natureza -->
-		<form enctype="multipart/form-data" autocomplete="off" class="mx-auto w-75" action="" method="post" id="form-inserir" name="form-inserir">            
+		<!-- O atributo enctype com o valor indicado é necessario quando queremos que o formulario aceite o envio de arquivos de qualquer natureza -->
+		<form enctype="multipart/form-data" autocomplete="off" class="mx-auto w-75" action="" method="post" id="form-inserir" name="form-inserir">
 
 			<div class="mb-3">
-                <label class="form-label" for="titulo">Título:</label>
-                <input class="form-control" required type="text" id="titulo" name="titulo" >
+				<label class="form-label" for="titulo">Título:</label>
+				<input class="form-control" required type="text" id="titulo" name="titulo">
 			</div>
 
 			<div class="mb-3">
-                <label class="form-label" for="texto">Texto:</label>
-                <textarea class="form-control" required name="texto" id="texto" cols="50" rows="6"></textarea>
+				<label class="form-label" for="texto">Texto:</label>
+				<textarea class="form-control" required name="texto" id="texto" cols="50" rows="6"></textarea>
 			</div>
 
 			<div class="mb-3">
-                <label class="form-label" for="resumo">Resumo (máximo de 300 caracteres):</label>
-                <span id="maximo" class="badge bg-danger">0</span>
-                <textarea class="form-control" required name="resumo" id="resumo" cols="50" rows="2" maxlength="300"></textarea> 
+				<label class="form-label" for="resumo">Resumo (máximo de 300 caracteres):</label>
+				<span id="maximo" class="badge bg-danger">0</span>
+				<textarea class="form-control" required name="resumo" id="resumo" cols="50" rows="2" maxlength="300"></textarea>
 			</div>
 
 			<div class="mb-3">
-                <label class="form-label" for="imagem" class="form-label">Selecione uma imagem:</label>
-                <input required class="form-control" type="file" id="imagem" name="imagem"
-                accept="image/png, image/jpeg, image/gif, image/svg+xml">
+				<label class="form-label" for="imagem" class="form-label">Selecione uma imagem:</label>
+				<input required class="form-control" type="file" id="imagem" name="imagem" accept="image/png, image/jpeg, image/gif, image/svg+xml">
 			</div>
-			
+
 
 			<button class="btn btn-primary" id="inserir" name="inserir"><i class="bi bi-save"></i> Inserir</button>
 		</form>
-		
+
 	</article>
 </div>
 
 
-<?php 
+<?php
 require_once "../inc/rodape-admin.php";
 ?>
-
