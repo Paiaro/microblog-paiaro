0d43581cd494d95ea68281092926b5e27b5c781f
Paiaro <gabrielpaiarop@gmail.com>
2024-06-12 13:45:36 -0300

    Update meu-perfil.php

--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -1,5 +1,20 @@
 <?php 
 require_once "../inc/cabecalho-admin.php";
+require_once "../inc/funcoes-usuarios.php";
+$id = $_SESSION['id'];
+$dadosDoUsuario = lerUmUsuario($conexao,$id);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $tipo = $_SESSION['tipo'];
+    if (empty($_POST['senha']) || password_verify($_POST['senha'], $dadosDoUsuario['senha'])) {
+        $senha = $dadosDoUsuario['senha'];
+    }else {
+        $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+    }
+    atualizarUsuario($conexao, $id, $nome, $email, $senha, $tipo);
+    header('Location:index.php');
+}
 ?>
 
 
@@ -14,12 +29,12 @@ require_once "../inc/cabecalho-admin.php";
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input value="<?=$dadosDoUsuario['nome']?>" class="form-control" type="text" id="nome" name="nome" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input value="<?=$dadosDoUsuario['email']?>" class="form-control" type="email" id="email" name="email" required>
 			</div>
 
 			<div class="mb-3">
